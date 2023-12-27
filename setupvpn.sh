da77973c374cf54eb594d884e20319f6584754f1
svtr <gazmeeva@mail.ru>
2023-12-27 13:42:26 +0300

    ДЗ Знакомство с облачной инфраструктурой Yandex.Cloud

--- setupvpn.sh
+++ setupvpn.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-echo "deb http://repo.pritunl.com/stable/apt focal main" | sudo tee /etc/apt/sources.list.d/pritunl.list
-apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+sudo tee /etc/apt/sources.list.d/pritunl.list << EOF
+deb http://repo.pritunl.com/stable/apt jammy main
+EOF
+
+# Import signing key from keyserver
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7568D9BB55FF9E5287D586017AE645C0CF8E292A
+# Alternative import from download if keyserver offline
 curl https://raw.githubusercontent.com/pritunl/pgp/master/pritunl_repo_pub.asc | sudo apt-key add -
-echo "deb https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/5.0 multiverse" | tee /etc/apt/sources.list.d/mongodb-o>wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
-apt update
-apt -y install wireguard wireguard-tools
-ufw disable
-apt -y install pritunl
-apt -y install mongodb-server
-systemctl enable mongodb pritunl
-systemctl start mongodb pritunl
+
+sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list << EOF
+deb https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/6.0 multiverse
+EOF
+
+wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+
+sudo apt update
+sudo apt --assume-yes upgrade
+
+# WireGuard server support
+sudo apt -y install wireguard wireguard-tools
+
+sudo ufw disable
+
+sudo apt -y install pritunl mongodb-org
+sudo systemctl enable mongod pritunl
+sudo systemctl start mongod pritunl

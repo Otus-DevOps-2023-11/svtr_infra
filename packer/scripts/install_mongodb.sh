205f6090a17036e291688d370279bf2768c0d8e4
svtr <gazmeeva@mail.ru>
2024-02-23 14:41:40 +0300

    ДЗ Ansible2

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-wget -qO - https://www.mongodb.org/static/pgp/server-3.2.asc | sudo apt-key add -
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-apt-get update
-apt install -y mongodb-org
-systemctl start mongod
-systemctl enable mongod
+apt update
+apt install mongodb -y
+systemctl status mongodb
+systemctl enable mongodb
